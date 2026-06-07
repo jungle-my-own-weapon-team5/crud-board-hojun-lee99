f4e1f8f159d3ae6a87b6c3310eb4f7d47f204521
hojun-lee99 <hijun990810@gmail.com>
2026-06-07 16:34:44 +0900

    chore: db connection  출력문 추가

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,6 +16,7 @@ def get_db_health(db: Session = Depends(get_db)):
     try:
         db.execute(text("SELECT 1"))
     except SQLAlchemyError:
+        print("/health/db: db connection error!")
         raise HTTPException(
             status_code=500,
             detail="Internal Error",
