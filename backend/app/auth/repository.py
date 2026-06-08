c733d647e2c16b61544c34eaa938b0fa7d2c7846
hojun-lee99 <hijun990810@gmail.com>
2026-06-08 20:43:17 +0900

    feat: user 생성 기능 구현

--- backend/app/auth/repository.py
+++ backend/app/auth/repository.py
@@ -8,3 +8,18 @@ from app.models import User
 def find_by_email(db: Session, email: str):
     return db.query(User).filter(User.email == email).first()
 
+def create_user(
+        db: Session,
+        email: str,
+        nickname: str,
+        hashed_password: str,
+) -> User:
+    user = User(
+        email=email,
+        nickname=nickname,
+        hashed_password=hashed_password,
+    )
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
