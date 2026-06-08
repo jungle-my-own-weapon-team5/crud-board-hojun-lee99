ec11cf7d4281eb32ead6ecdcba6e357bb9441b81
hojun-lee99 <hijun990810@gmail.com>
2026-06-08 20:40:50 +0900

    chore: main api 라우팅 추가

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,8 +4,11 @@ from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
 
 from app.database import get_db
+from app.api import api_router
 
 app = FastAPI()
+
+app.include_router(api_router)
 
 @app.get("/health")
 def read_root():
