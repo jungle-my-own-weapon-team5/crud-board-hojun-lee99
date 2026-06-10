aa90da1dfec9ee799f440c21e4c62be9a8baefa0
hojun-lee99 <hijun990810@gmail.com>
2026-06-10 10:57:13 +0900

    feat: 게시글 리스트 조회 라우터 구현

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from app.auth.router import router as auth_router
+from app.posts.router import router as posts_router
 
 api_router = APIRouter()
 
 api_router.include_router(auth_router, prefix='/auth', tags=["auth"])
+api_router.include_router(posts_router, prefix='/posts', tags=["posts"])
