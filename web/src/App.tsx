3995c0500b09f51b1e4d2ad2a47356f1691aaa52
hojun-lee99 <hijun990810@gmail.com>
2026-06-10 17:01:58 +0900

    feat: 로그인 페이지 연결 추가

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,24 @@
+import { useState } from 'react'
 import './App.css'
 import LoginPage from './LoginPage'
 import PostListPage from './PostListPage'
 import RegisterPage from './RegisterPage'
+import PostCreatePage from './PostCreatePage'
+
+type Page = 'list' | 'create'
 
 function App() {
-  return <PostListPage />
+  const [page, setPage] = useState<Page>('list')
+  
+  if (page === 'create') {
+    return (
+      <PostCreatePage
+        onCreated={() => setPage('list')}
+        onCancel={() => setPage('list')}
+      />
+    )
+  }
+  return <PostListPage onCreateClick={() => setPage('create')} />
 }
 
 export default App
