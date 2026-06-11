5925cf8381cea9e3ffbb6b58be9ea5539906b6db
hojun-lee99 <hijun990810@gmail.com>
2026-06-11 20:10:15 +0900

    db: add boards table

--- backend/app/models.py
+++ backend/app/models.py
@@ -9,6 +9,7 @@ class Post(Base):
     __tablename__ = "posts"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    board_id: Mapped[int] = mapped_column(Integer, nullable=False)
     user_id: Mapped[int] = mapped_column(Integer, nullable=False)
     title: Mapped[str] = mapped_column(String(100), nullable=False)
     content: Mapped[str] = mapped_column(Text, nullable=False)
@@ -33,6 +34,12 @@ class Post(Base):
         primaryjoin=lambda: foreign(Post.user_id) == User.id,
         back_populates="posts",
     )
+    board: Mapped["Post"] = relationship(
+        "Board",
+        primaryjoin=lambda: foreign(Post.board_id) == Board.id,
+        back_populates="posts",
+    )
+    
 
 class User(Base):
     __tablename__ = "users"
@@ -42,24 +49,28 @@ class User(Base):
     email: Mapped[str] = mapped_column(String(255), unique=True, index=True, nullable=False)
     hashed_password: Mapped[str] = mapped_column(String(255), nullable= False)
     
-    created_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True),
-        server_default=func.now(),
-        nullable=False
-    )
-    updated_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True),
-        server_default=func.now(),
-        onupdate=func.now(),
-        nullable=False
-    )
-    deleted_at: Mapped[datetime | None] = mapped_column(
-        DateTime(timezone=True),
-        nullable=True,
-    )
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+    deleted_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), nullable=True)
 
     posts: Mapped[list["Post"]] = relationship(
         "Post",
         primaryjoin=lambda: User.id == foreign(Post.user_id),
         back_populates="user",
+    )
+
+class Board(Base):
+    __tablename__ = "boards"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[str | None] = mapped_column(Text, nullable=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+    deleted_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), nullable=True)
+    
+    posts: Mapped[list["Post"]] = relationship(
+        "Post",
+        primaryjoin=lambda: Board.id == foreign(Post.board_id),
+        back_populates="board",
     )
