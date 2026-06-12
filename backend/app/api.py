9ba175fbf9f92097047db8794d4d63d0d80c966d
hojun-lee99 <hijun990810@gmail.com>
2026-06-12 11:13:18 +0900

    chore: boards api 라우팅 추가

--- backend/app/api.py
+++ backend/app/api.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from app.auth.router import router as auth_router
 from app.posts.router import router as posts_router
+from app.boards.router import router as boards_router
 
 api_router = APIRouter()
 
 api_router.include_router(auth_router, prefix='/auth', tags=["auth"])
 api_router.include_router(posts_router, prefix='/posts', tags=["posts"])
+api_router.include_router(boards_router, prefix='/boards', tags=["boards"])
