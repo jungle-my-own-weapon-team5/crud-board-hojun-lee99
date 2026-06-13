2520f5060d5f76d241384701d288a0cdfae36410
hojun-lee99 <hijun990810@gmail.com>
2026-06-13 13:13:07 +0900

    type: comment 생성 dto 작성

--- backend/app/comments/schemas.py
+++ backend/app/comments/schemas.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, field_validator
 
 class CommentListItem(BaseModel):
     id: int
@@ -8,10 +8,24 @@ class CommentListItem(BaseModel):
     content: str
     created_at: datetime
     updated_at: datetime
-    deleted_at: datetime
+    deleted_at: datetime | None
 
     model_config = ConfigDict(from_attributes=True)
     
 class CommentListResponse(BaseModel):
     items: list[CommentListItem]
     total: int
+
+class CommentCreateRequest(BaseModel):
+    content: str
+
+    @field_validator("content")
+    @classmethod
+    def validate_content(cls, value:str):
+        content = value.strip()
+        if not content:
+            raise ValueError("댓글 내용을 입력해주세요.")
+        return content
+    
+class CommentCreateResponse(CommentListItem):
+    pass
